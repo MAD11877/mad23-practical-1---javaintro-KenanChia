72ac6423e5038feb2e0138e04b3a1bec8d0d050a
Kenan Chia <Kenan Chia@LAPTOP-04G3VC6V>
2023-04-20 15:26:13 +0800

    commit

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter an integer ");
+    /*System.out.print("Enter an integer ");*/
     double number = in.nextDouble();
     number *= number;
     System.out.println(number);
